a5977020ed7ad17e5dd4b59890a14278a3b52154
Alan Anderson <alanpurple@gmail.com>
2021-06-25 19:07:33 +0900

    add cleansing api

---
+++ MLServer/connectdb.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+from sqlalchemy.ext.automap import automap_base
+from urllib import parse
+
+server='localhost'
+database='hyperai'
+driver = 'MySQL ODBC 8.0 Unicode Driver'
+id='root'
+pwd='alan1234'
+
+def getAll():
+    engine=create_engine("mysql+mysqlconnector://{}:{}@{}/{}".format(id,pwd,server,database))
+    #engine=create_engine('mysql+pyodbc://{}:{}@{}/{}?driver={}'.format(id,pwd,server,database,parse.quote_plus(driver)))
+    base = automap_base()
+    base.prepare(engine,reflect=True)
+    return engine.connect(),scoped_session(sessionmaker(engine)),base
--- MLServer/eda_service.py
+++ MLServer/eda_service.py
@@ -2,12 +2,72 @@ from sklearn.decomposition import PCA
 import numpy as np
 import pandas as pd
 
+from connectdb import getAll
+
 import eda_pb2
 import eda_pb2_grpc
 
 class EdaService(eda_pb2_grpc.PreprocessServicer):
     def Cleanse(self, request, context):
-        return super().Cleanse(request, context)
+        if request.location =='':
+            return eda_pb2.CleanseReply(error=0)
+        conn, session, Base = getAll()
+        data=Base.classes[request.location]
+        q=session.query(data)
+        session.close()
+        df=pd.read_sql(q.statement,conn)
+        for label,content in df.items():
+            if pd.api.types.is_strng_dtype(content):
+                hasemptystr=(content.str.len<1).all()
+                if content[content.str.len>0].str.isnumeric.all():
+                    new_content=pd.to_numeric(content)
+            else:
+
+
 
     def Describe(self, request, context):
-        return super().Describe(request, context)
+        if request.location =='':
+            return eda_pb2.CleanseReply(error=0)
+        conn, session, Base = getAll()
+        data=Base.classes[request.location]
+        q=session.query(data)
+        session.close()
+        df=pd.read_sql(q.statement,conn)
+        obj=df.describe(include='all').to_dict()
+        del obj['id']
+        result=[]
+        for key,data in obj.items():
+            temp={'name':key, 'count':int(data['count'])}
+            currentData=df.loc[:,key]
+
+            # normal categorical attribute
+            if 'unique' in data and not numpy.isnan(data['unique']):
+                temp['type']='categorical'
+                temp['unique']=int(data['unique'])
+                if not isinstance(data['top'],float):
+                    if isinstance(data['top'],datetime.date):
+                        temp['top']= data['top'].strftime('%Y-%m-%d %H:%M')
+                        temp['type']='datetime'
+                    else:
+                        temp['top']=data['top']
+                if not numpy.isnan(data['freq']):
+                    temp['freq']=int(data['freq'])
+            # numerical, but needs to be categorical
+            elif len(numpy.unique(currentData))<20:
+                temp['type']='categorical'
+                x,y=numpy.unique(currentData,return_counts=True)
+                temp['unique']=len(x)
+                temp['freq']=max(y)
+                temp['top']=str(x[numpy.argmax(y)])
+            # normal numerical
+            else:
+                temp['type']='numeric'
+                temp['q1']=data['25%']
+                temp['q2']=data['50%']
+                temp['q3']=data['75%']
+                temp['mean']=data['mean']
+                temp['min']=data['min']
+                temp['max']=data['max']
+                temp['std']=data['std']
+            result.append(temp)
+            return eda_pb2.SummaryReply(summaries=result)
