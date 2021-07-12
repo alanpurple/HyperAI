3630f8bf73f091c97409734ece7c0ab8b51cd451
Alan Anderson <alanpurple@gmail.com>
2021-07-12 20:34:47 +0900

    add data service

--- MLServer/data_service.py
+++ MLServer/data_service.py
@@ -1,1 +1,22 @@
+import pandas as pd
+import numpy as np
 
+import data_service_pb2
+import data_service_pb2_grpc
+
+class DataServicer(data_service_pb2_grpc.DataServicer):
+    def Upload(self, request, context):
+        if request.name=='':
+            return data_service_pb2.UploadResponse(error=1)
+        filename=request.name
+        # header should be on the first row
+        path='../WebServer/upload-temp'
+        ext=filename.split('.')[-1]
+        f=open(path+filename,'r')
+
+        if ext=='csv':
+            df=pd.read_csv(f)
+        elif ext=='xlsx':
+            df=pd.read_excel(f)
+
+        f.close()
