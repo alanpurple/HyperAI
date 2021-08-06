45cffcb1aff20dbbb94867a04d0de1ddd0f35575
Alan Anderson <alanpurple@gmail.com>
2021-08-06 17:37:12 +0900

    add association

--- HyperAI/src/app/association.ts
+++ HyperAI/src/app/association.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { PieData, BoxPlotData, ScatterData, PlotData } from 'plotly.js';
 
 import { DataService } from './data.service';
+import { ErrorAlert } from './error.alert';
 
 @Component({
   templateUrl: './association.html'
 })
 export class Association implements OnInit {
   constructor(
-    private dataService: DataService
+    private dataService: DataService,
+    private errorAlert: ErrorAlert
   ) {
 
   }
 
   tableNames: string[] = [];
+
+  associationData1: PlotData[] = [];
+  associationData2: PieData[] = [];
+  boxPlotData: BoxPlotData[] = [];
+  lrData = [];
+
 
   ngOnInit() {
     this.dataService.getDataMy().subscribe(datalist => {
