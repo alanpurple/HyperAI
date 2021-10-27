5e02954e70e9125b6ca540315763eb412428da91
Alan Anderson <alanpurple@gmail.com>
2021-10-27 15:50:20 +0900

    update timestamp writing

--- WebServer/models/data.info.ts
+++ WebServer/models/data.info.ts
@@ -2,6 +2,7 @@ import { Schema } from 'mongoose';
 
 export interface DataInfo {
     name: string;   // local folder name for local type, URI elsewhere
+    createdAt: Date;
     locationType: 'db uri' | 'local' | 'smb' | 'datalake' | 'aws s3';
     type: 'structural' | 'sound' | 'text' | 'image';
     numRows: number;
@@ -12,20 +13,23 @@ export interface DataInfo {
 
 export interface DataBasic {
     name: string;
+    createdAt: Date;
     numRows: number;
 }
 
 const BasicSchema = new Schema<DataBasic>({
     name: { type: String, unique: true, required: true },
+    createdAt: Date,
     numRows: { type: Number, required: true }
-}, { _id: false, autoIndex: false });
+}, { _id: false, autoIndex: false, timestamps: true });
 
 export const DataSchema = new Schema<DataInfo>({
     name: { type: String, unique: true, required: true },
     locationType: { type: String, enum: ['db uri', 'local', 'smb', 'datalake', 'aws s3'], required: true },
+    createdAt: Date,
     type: { type: String, enum: ['structural', 'sound', 'text', 'image'], required: true },
     numRows: { type: Number, required: true },
     isClean: Boolean,
     cleansed: BasicSchema,
     preprocessed: BasicSchema
-}, { _id: false, autoIndex: false });
+}, { _id: false, autoIndex: false, timestamps: true });
