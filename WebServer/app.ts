d38525e680179763a491a5ccd3383a03534ec037
Alan Anderson <alanpurple@gmail.com>
2021-11-03 17:43:31 +0900

    change app.listen to socketserver.listen

--- WebServer/app.ts
+++ WebServer/app.ts
@@ -157,6 +157,6 @@ app.use((err, req, res, next) => { // eslint-disable-line @typescript-eslint/no-
 
 app.set('port', process.env.PORT || 3000);
 
-const server = app.listen(app.get('port'), function () {
-    debug(`Express server listening on port ${(server.address() as AddressInfo).port}`);
-});
+const server = socketServer.listen(app.get('port'), () =>
+    debug(`Express server listening on port ${(server.address() as AddressInfo).port}`)
+);
