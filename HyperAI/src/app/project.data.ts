2db10695f32b865d73fb0aa810bccdc7a5c14d58
Alan Anderson <alanpurple@gmail.com>
2021-11-08 19:41:33 +0900

    project taskType->category

--- HyperAI/src/app/project.data.ts
+++ HyperAI/src/app/project.data.ts
@@ -2,7 +2,7 @@ export interface Project {
   name: string;
   dataURI: string;
   projectType: 'single' | 'sequential' | 'multiple_comparison';
-  taskType: 'various' | 'vision' | 'text' | 'structural';
+  category: 'various' | 'vision' | 'text' | 'structural';
   owner: string;  // self for user, email for admin
   members: [{ user: string /*email*/, role: 'attendee' | 'member' }]; // one and only owner, others are all attendee(for now)
   visionTasks: VisionTask[];  // use only when taskType is 'vision'
