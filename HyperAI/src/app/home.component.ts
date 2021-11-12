2eccd5d36a8150ef6d993305b3bb79cc8f4704bb
Alan Anderson <alanpurple@gmail.com>
2021-11-12 18:31:55 +0900

    update log in check

--- HyperAI/src/app/home.component.ts
+++ HyperAI/src/app/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ErrorAlert } from './error.alert';
 import { Project } from './project.data';
 import { ProjectService } from './project.service';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +13,24 @@ export class HomeComponent implements OnInit {
 
   constructor(
     private projectService: ProjectService,
+    private userService: UserService,
     private errorAlert: ErrorAlert
   ) { }
 
   projects: Project[] = [];
+  loggedIn: boolean = false;
   
   ngOnInit(): void {
-    this.projectService.getProjects().subscribe(
-      projects => this.projects = projects,
-      err => {
-        if (err.status != 404)
-          this.errorAlert.open(err);
+    this.userService.getUser().subscribe(
+      user => {
+        this.loggedIn = true;
+        this.projectService.getProjects().subscribe(
+          projects => this.projects = projects,
+          err => {
+            if (err.status != 404)
+              this.errorAlert.open(err);
+          }
+        );
       }
     );
   }
